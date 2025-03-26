de7b754de0964bb8c02c4723dfc629a58ed97b6e
wonchul08.lee <wonchul08.lee@gmail.com>
2025-03-26 17:07:27 +0900

    Add getGop() method and test case

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -3,11 +3,24 @@
 class Cal {
 public:
 	// 이곳에 제작
+	int getGop(int a, int b) { 
+		return a * b;
+	}
+
 };
 //테케 추가
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
 }
+
+TEST(t1, gopTest) {
+	Cal cal;
+	int actual = cal.getGop(3, 5);
+
+	EXPECT_EQ(15, actual);
+}
+
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
