42811d0811676d02ae7b04be45ce4732fad8c7c3
Bongsu Jeon <bs.jeon87@gmail.com>
2025-03-26 17:06:23 +0900

    Add the minus function

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -3,11 +3,21 @@
 class Cal {
 public:
 	// 이곳에 제작
+	int minus(int firtValue, int secondValue) {
+		return firstValue - secondValue;
+	}
 };
 //테케 추가
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
 }
+
+//테케 추가
+TEST(Cal_Minus, minusTc1) {
+	Cal cal;
+	EXPECT_EQ(2, cal.minus(4,2));
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
