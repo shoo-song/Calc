bc22b7079734d090b831439e93e6a9ee6ffddc0a
shoo.song <shoo.song@samsung.com>
2025-03-26 17:07:32 +0900

    add enable

--- Project4/main.cpp
+++ Project4/main.cpp
@@ -3,10 +3,15 @@
 class Cal {
 public:
 	// 이곳에 제작
+	int Add(int a, int b) {
+		return a + b;
+	}
 };
 //테케 추가
-TEST(t1, t2) {
-	EXPECT_EQ(1, 1);
+TEST(Cal, AddTest) {
+	Cal C;
+	int result = C.Add(3, 5);
+	EXPECT_EQ(result, 8);
 }
 int main() {
 	::testing::InitGoogleMock();
